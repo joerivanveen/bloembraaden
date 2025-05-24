7278991f291cd3ae61319f3aa7fea13fd2b933fc
joerivanveen <nonstockphoto@gmail.com>
2025-05-24 08:06:17 +0200

    fix(account): compare addresses better

--- bloembraaden/Job.php
+++ bloembraaden/Job.php
@@ -139,19 +139,20 @@ switch ($interval) {
                 // 1) mail order confirmation
                 if (false === $row->emailed_order_confirmation) {
                     if (0 !== $row->user_id) {
+                        // todo 0.26.0 this must be done also when the account is created after ordering!
                         echo "Check address for account\n";
+                        // todo have configurable shop addresses appear in the hashes, to not add the ‘collect’ address to the account
+                        $by_hash = array(md5('1402abhuizerweg22nl') => 'Collect');
                         // get all addresses for this user_id, md5 them
                         $addresses = $db->fetchAddressesByUserId($row->user_id);
-                        $by_hash = array();
                         foreach ($addresses as $index => $address) {
-                            $hash = md5("$address->address_postal_code$address->address_street$address->address_number$address->address_number_addition$address->address_country_iso2");
+                            $hash = md5(strtolower(str_replace(' ', '', "$address->address_postal_code$address->address_street$address->address_number$address->address_number_addition$address->address_country_iso2")));
                             $by_hash[$hash] = $address;
                         }
-                        // todo have the shop addresses appear in the hashes, to not add the ‘collect’ address to the account
                         $addresses = null;
                         // get shipping / billing address -> md5, if not in the addresses md5, add it
                         foreach (array('billing', 'shipping') as $index => $address_type) {
-                            $hash = md5($row->{"{$address_type}_address_postal_code"} . $row->{"{$address_type}_address_street"} . $row->{"{$address_type}_address_number"} . $row->{"{$address_type}_address_number_addition"} . $row->{"{$address_type}_address_country_iso2"});
+                            $hash = md5(strtolower(str_replace(' ', '', $row->{"{$address_type}_address_postal_code"} . $row->{"{$address_type}_address_street"} . $row->{"{$address_type}_address_number"} . $row->{"{$address_type}_address_number_addition"} . $row->{"{$address_type}_address_country_iso2"})));
                             if (false === isset($by_hash[$hash])) {
                                 $address = array(
                                     'user_id' => $row->user_id,
